28933a30d6808869e6564e6fbdc1514b66486e56
VDMHKS <140140069+VDMHKS@users.noreply.github.com>
2023-09-13 15:05:24 +0900

    Add files via upload

--- js/pick.js
+++ js/pick.js
@@ -1,14 +1,14 @@
 $(function () {
     $.ajax({
-        url: "./json/pick.json",
+        url: "../json/pick.json",
         dataType: "json",
         success: function (data) {
             if (data.length > 0) {
                 for (var i in data) {
-                    $('.imgbox').eq(i).find('img').attr('src', data[i].image)
-                    $('.description').eq(i).prepend('<span>'+data[i].brand+'</span>');
-                    $('.description').eq(i).append('<h6><a href="sub.html" target="_self">'+data[i].product+'</a></h6>');
-                    $('.description').eq(i).append('<h7>'+data[i].price+'</h7>');
+                    $('.box5 .imgbox').eq(i).find('img').attr('src', data[i].image)
+                    $('.box5 .description').eq(i).prepend('<span>'+data[i].brand+'</span>');
+                    $('.box5 .description').eq(i).append('<h6><a href="sub.html" target="_self">'+data[i].product+'</a></h6>');
+                    $('.box5 .description').eq(i).append('<h7>'+data[i].price+'</h7>');
                 }
             }
         }
