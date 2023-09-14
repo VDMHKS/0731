081479f22481edcc432dde57412ba2ac47726342
VDMHKS <140140069+VDMHKS@users.noreply.github.com>
2023-09-14 15:32:58 +0900

    Add files via upload

--- js/knock.js
+++ js/knock.js
@@ -6,8 +6,8 @@ $(function () {
             if (data.length > 0) {
                 for (var i in data) {
                     $('.seventbox a .eventimg').eq(i).append('<img src="'+data[i].image+'" alt="이벤트">');
-                    $('.seventbox a .eventinfo').eq(i).append('<p> <span>'+data[i].line1+'</span>');
-                    $('.seventbox a .eventinfo').eq(i).append(data[i].line2+'</p>');
+                    $('.seventbox a .eventinfo').eq(i).append('<span>'+data[i].line1+'</span>');
+                    $('.seventbox a .eventinfo').eq(i).append('<p>'+data[i].line2+'</p>');
                 }
             }
         }
