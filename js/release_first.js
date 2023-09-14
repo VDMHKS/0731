063bf83bdb21589ffb2acdafcc8aa97f5cbef09a
VDMHKS <140140069+VDMHKS@users.noreply.github.com>
2023-09-14 12:48:40 +0900

    Add files via upload

---
+++ js/release_first.js
@@ -0,0 +1,16 @@
+$(function () {
+    $.ajax({
+        url: "./json/release_first.json",
+        dataType: "json",
+        success: function (data) {
+            if (data.length > 0) {
+                for (var i in data) {
+                    $('#box1 #release_wrap1 .product .photo').eq(i).append('<a href="sub.html" target="_self"><img src="'+data[i].image+'" alt="product"></a>');
+                    $('#box1 #release_wrap1 .product .description').eq(i).prepend('<span>'+data[i].brand+'</span>');
+                    $('#box1 #release_wrap1 .product .description').eq(i).append('<h6><a href="sub.html" target="_self">'+data[i].product+'</a></h6>');
+                    $('#box1 #release_wrap1 .product .description').eq(i).append('<h7>'+data[i].price+'</h7>');
+                };
+            }
+        }
+    });
+});
