84342fe538543af25f7588b455d3e3c2d04d6dac
VDMHKS <140140069+VDMHKS@users.noreply.github.com>
2023-09-14 09:30:15 +0900

    Add files via upload

--- js/release_first.js
+++ js/release_first.js
@@ -9,13 +9,13 @@ $(function () {
                     $('#box1 #first').eq(i).prepend('<h5>'+data[i].line1+'</h5>');
                     $('#box1 #first').eq(i).prepend('<img src="'+data[i].image+'" alt="photo">');
                 };
-                for (var i=2; 2<=i>4; i++) {
+                for (var i=2; i<4; i++) {
                     $('#box1 #first .product .photo').eq(i).append('<a href="sub.html" target="_self"><img src="'+data[i].image+'" alt="product"></a>');
                     $('#box1 #first .product .description').eq(i).prepend('<span>'+data[i].brand+'</span>');
                     $('#box1 #first .product .description').eq(i).append('<h6><a href="sub.html" target="_self">'+data[i].product+'</a></h6>');
                     $('#box1 #first .product .description').eq(i).append('<h7>'+data[i].price+'</h7>');
                 };
-                for (var i=4; 4<=i>5; i++) {
+                for (var i=4; i<5; i++) {
                     $('#box1 #first .product2 .photo').eq(i).append('<a href="sub.html" target="_self"><img src="'+data[i].image+'" alt="product"></a>');
                     $('#box1 #first .product2 .description').eq(i).prepend('<span>'+data[i].brand+'</span>');
                     $('#box1 #first .product2 .description').eq(i).append('<h6><a href="sub.html" target="_self">'+data[i].product+'</a></h6>');
--- js/release_third.js
+++ js/release_third.js
@@ -9,7 +9,7 @@ $(function () {
                     $('#box1 #third').eq(i).prepend('<h5>'+data[i].line1+'</h5>');
                     $('#box1 #third').eq(i).prepend('<img src="'+data[i].image+'" alt="photo">');
                 };
-                for (var i=2; 2<=i>4; i++) {
+                for (var i=2; i<4; i++) {
                     $('#box1 #third .product .photo').eq(i).append('<a href="sub.html" target="_self"><img src="'+data[i].image+'" alt="product"></a>');
                     $('#box1 #third .product .description').eq(i).prepend('<span>'+data[i].brand+'</span>');
                     $('#box1 #third .product .description').eq(i).append('<h6><a href="sub.html" target="_self">'+data[i].product+'</a></h6>');
