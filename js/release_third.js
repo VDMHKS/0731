43e50dddc516657d5b8594dd586904ea51761efc
VDMHKS <140140069+VDMHKS@users.noreply.github.com>
2023-09-14 10:06:03 +0900

    Add files via upload

--- js/release_third.js
+++ js/release_third.js
@@ -4,17 +4,12 @@ $(function () {
         dataType: "json",
         success: function (data) {
             if (data.length > 0) {
-                for (var i=1; i<2; i++) {
+                for (var i in data) {
                     $('#box1 #third').eq(i).prepend('<p>'+data[i].line2+'</p>');
                     $('#box1 #third').eq(i).prepend('<h5>'+data[i].line1+'</h5>');
                     $('#box1 #third').eq(i).prepend('<img src="'+data[i].image+'" alt="photo">');
                 };
-                for (var j=2; j<4; j++) {
-                    $('#box1 #third .product .photo').eq(j).append('<a href="sub.html" target="_self"><img src="'+data[j].image+'" alt="product"></a>');
-                    $('#box1 #third .product .description').eq(j).prepend('<span>'+data[j].brand+'</span>');
-                    $('#box1 #third .product .description').eq(j).append('<h6><a href="sub.html" target="_self">'+data[j].product+'</a></h6>');
-                    $('#box1 #third .product .description').eq(j).append('<h7>'+data[j].price+'</h7>');
-                };
+                
             }
         }
     });
