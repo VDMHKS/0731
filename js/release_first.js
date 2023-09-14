9509203ee1cd7fdc4b79a4c631c1c00e50029fb4
VDMHKS <140140069+VDMHKS@users.noreply.github.com>
2023-09-14 10:10:52 +0900

    Update release_first.js

--- js/release_first.js
+++ js/release_first.js
@@ -9,7 +9,12 @@ $(function () {
                     $('#box1 #first').eq(i).prepend('<h5>'+data[i].line1+'</h5>');
                     $('#box1 #first').eq(i).prepend('<img src="'+data[i].image+'" alt="photo">');
                 };
-                
+                for (var j=2; j<4; j++) {
+                    $('#box1 #first .product .photo').eq(j).append('<a href="sub.html" target="_self"><img src="'+data[j].image+'" alt="product"></a>');
+                    $('#box1 #first .product .description').eq(j).prepend('<span>'+data[j].brand+'</span>');
+                    $('#box1 #first .product .description').eq(j).append('<h6><a href="sub.html" target="_self">'+data[j].product+'</a></h6>');
+                    $('#box1 #first .product .description').eq(j).append('<h7>'+data[j].price+'</h7>');
+                };
             }
         }
     });
